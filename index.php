6af8e5afde1b0a60e25dade0f51139088ea0eda6
DSlite <setyadharma.dindra@gmail.com>
2019-10-16 16:04:32 +0700

    Shared Header & Footer

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+  include 'function.php';
+  template_header("E-Learning Academy");
+?>
+
+    <section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/big_image_1.jpg);">
+      <div class="container">
+        <div class="row align-items-center site-hero-inner justify-content-center">
+          <div class="col-md-8 text-center">
+
+            <div class="mb-5 element-animate">
+              <h1 style="font-size: 50px">LEARNING EVERYWHERE</h1>
+              <p class="lead">Learn, Do Homework, Upload File, and Everything in one Website</p>
+              <p><a href="dashboard/index.html" class="btn btn-primary">Login or Sign Up</a></p>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+    <section class="school-features d-flex" style="background-image: url(images/big_image_3.svg);">
+
+      <div class="inner">
+        <div class="media d-block feature">
+          <div class="icon"><span class="flaticon-video-call"></span></div>
+          <div class="media-body">
+            <h3 class="mt-0">Online trainings from experts</h3>
+            <p>Web conference with your Professor and learn it all!</p>
+          </div>
+        </div>
+
+        <div class="media d-block feature">
+          <div class="icon"><span class="flaticon-student"></span></div>
+          <div class="media-body">
+            <h3 class="mt-0">Learn anywhere in the world</h3>
+            <p>With all your device like handphone, laptop and computer you can learn almost everywhere. If you have an Internet!</p>
+          </div>
+        </div>
+
+        <div class="media d-block feature">
+          <div class="icon"><span class="flaticon-video-player-1"></span></div>
+          <div class="media-body">
+            <h3 class="mt-0">Creative learning video</h3>
+            <p>Learning is Fun with Video!</p>
+          </div>
+        </div>
+
+
+        <div class="media d-block feature">
+          <div class="icon"><span class="flaticon-audiobook"></span></div>
+          <div class="media-body">
+            <h3 class="mt-0">Audio learning</h3>
+            <p>Announce from your Professor, even Chat with him/her!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+ 
+ <?php template_footer();?>
