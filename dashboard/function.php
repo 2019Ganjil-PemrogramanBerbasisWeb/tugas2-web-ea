04f96035ca3ac1d242c2776895ea22fe97285401
Ilya Asha <ilyaasha24@gmail.com>
2019-10-22 18:33:02 +0700

    Update function.php

--- dashboard/function.php
+++ dashboard/function.php
@@ -22,23 +22,23 @@ function pdo_connect_mysql() {
     }
 }
 function dashboard_header($title, $session_name) {
-  echo <<<EOT
-<!DOCTYPE html>
-<html>
-	<head>
-  	<meta charset="utf-8">
-  	<title>$title</title>
-  	<link href="style.css" rel="stylesheet" type="text/css">
-  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-  </head>
-  <body class="loggedin">
-  	<nav class="navtop">
-    	<div>
-    		<h1><a href="home.php"><i class="fas fa-percentage"></i>$session_name</a></h1>
-    		<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-    		<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-    	</div>
-		</nav>
+	echo <<<EOT
+  <!DOCTYPE html>
+  <html>
+  	<head>
+  		<meta charset="utf-8">
+  		<title>$title</title>
+      <link href="style.css" rel="stylesheet" type="text/css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body class="loggedin">
+      <nav class="navtop">
+        <div>
+          <h1><a href="home.php"><i class="fas fa-percentage"></i>$session_name</a></h1>
+          <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+          <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+        </div>
+      </nav>
   EOT;
 }
 function dashboard_footer() {
