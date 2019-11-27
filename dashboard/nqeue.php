60488517b66448c5f7e899154016e03f1a2fedfe
ilyaasha24 <55181949+ilyaasha24@users.noreply.github.com>
2019-11-27 16:35:03 +0700

    Update nqeue.php

--- dashboard/nqeue.php
+++ dashboard/nqeue.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 $dbname = $_GET['n'];
 $query = $_GET['q'];
 // Create connection
